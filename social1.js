7d2acb9d8ec7d1c00973683e25044cb7d3db50de
SushiSalmonDev <108323624+SalmonSushiDev@users.noreply.github.com>
2022-11-30 15:39:16 +0800

    Add files via upload

---
+++ social1.js
@@ -0,0 +1,16 @@
+//Functions#1//
+    //YT
+function linkYT1(){window.open("https://www.youtube.com/channel/UCisWKpq78rbAwD6RWaSbsag", "_blank");}
+function linkYT2(){window.open("https://www.youtube.com/@aprill_ed1tz446", "_blank");}
+function linkYT3(){window.open("https://www.youtube.com/@aprillanimates8068", "_blank");}
+function linkYT4(){window.open("https://www.youtube.com/@lilythesupercat3741", "_blank");}
+
+    //Rbx
+function linkRbx1(){window.open("https://www.roblox.com/users/3393786005/profile", "_blank");}
+function linkRbx2(){window.open("https://www.roblox.com/users/1714970723/profile", "_blank");}
+function linkRbx3(){window.open("https://www.roblox.com/users/3119765963/profile", "_blank");}
+function linkRbx4(){alert("Coming soon!1!1 (Sorry, I'm lazy)");}
+
+    //Codes
+function linkCode1(){window.open("https://github.com/SalmonSushiDev", "_blank");}
+function linkCode2(){window.open("https://codepen.io/SalmonSushiDev", "_blank");}
